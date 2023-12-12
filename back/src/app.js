486b0991065600d853ff3ca262534bb3af714153
BDora <97038220+N2InfraestruturaSB@users.noreply.github.com>
2023-12-11 23:13:13 -0300

    mongodb

--- back/src/app.js
+++ back/src/app.js
@@ -2,8 +2,11 @@ const express = require('express')
 const cors = require('cors')
 const router = require('./router')
 const app = express()
-// const mustacheExpress = require ('mustache-express')
-// const engine = mustacheExpress()
+const  mongoose = require ('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const mustacheExpress = require ('mustache-express')
+const engine = mustacheExpress()
 
 
 // app.engine("mustache", engine)
@@ -12,9 +15,15 @@ const app = express()
 
 // app.get('/', (req,res) => {res.render("home",{title: "Ola"})})
 
+//const dbUser = process.env.DB_USER
+//const dbPass = process.env.DB_PASS
+
+mongoose.connect('mongodb+srv://beatrizdoraw:NpGuwA71g9ePGzfB@cluster0.5zhqw2v.mongodb.net/?retryWrites=true&w=majority').then(() => {
+console.log('onde eu to?')
+
+}).catch((err) => console.log(err))
 app.use(express.json())
 app.use(router)
 app.use(cors())
-
 module.exports = app;
 // 1 jan 1970
